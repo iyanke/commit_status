3d3e2e61c895dac34802b7964b5d75beda9b9aa7
63282359+iyanke <63282359+iyanke@TeamCity>
2021-07-20 16:11:29 +0300

    TeamCity change in 'Pull Requests Plugin / Commit Status Publisher' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,58 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2021.1"
+
+project {
+
+    buildType(GitHub)
+}
+
+object GitHub : BuildType({
+    name = "GitHub"
+
+    vcs {
+        root(AbsoluteId("PullRequestsPlugin_HttpsGithubComIyankeTests"))
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        commitStatusPublisher {
+            vcsRootExtId = "PullRequestsPlugin_HttpsGithubComIyankeTests"
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "zxx70612cf7d0faead55254cb740f4374977cfb90553eab154064c1df14a1aa783eafbb95d2a1c13585775d03cbe80d301b"
+                }
+            }
+            param("github_oauth_user", "iyanke")
+        }
+    }
+})
